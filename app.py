e24df88c4d97d0e7d7c28cb6a8183174e4fe45cf
happylulu <happylulu421@gmail.com>
2024-08-07 18:23:01 -0700

    Update app.py
    update prompt for coaching

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import os
 
 
 CLIP_PATH = "google/siglip-so400m-patch14-384"
-VLM_PROMPT = "A descriptive caption for this image:\n"
+VLM_PROMPT = "Asuming you are a world class bodybuilding coach. A descriptive caption for this image, on muscule development, codition and progress:\n"
 MODEL_PATH = "meta-llama/Meta-Llama-3.1-8B"
 CHECKPOINT_PATH = Path("wpkklhc6")
 TITLE = "<h1><center>JoyCaption Pre-Alpha (2024-07-30a)</center></h1>"
